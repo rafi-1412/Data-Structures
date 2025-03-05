ea956d3132040eb9e7bb5e3653e54023d79694e2
Shaik Mohammad, Rafi <Rafi.ShaikMohammad@harman.com>
2025-03-05 16:41:31 +0530

    [threading] C++ advanced topics

---
+++ c++_advanced/unique_lock.cpp
@@ -0,0 +1,58 @@
+// Few important points to remember about the unique_lock is as follows:
+// 1. The class unique_lock is a mutex ownership wrapper.
+// 2. It Allows:
+//     a. Can Have Different Locking Strategies
+//     b. time-constrained attempts at locking (try_lock_for, try_lock_until)
+//     c. recursive locking
+//     d. transfer of lock ownership (move not copy)
+//     e. condition variables. (See this in coming videos)
+
+// Locking Strategies
+//    TYPE           EFFECTS(S)
+// 1. defer_lock  do not acquire ownership of the mutex.
+// 2. try_to_lock  try to acquire ownership of the mutex without blocking.
+// 3. adopt_lock  assume the calling thread already has ownership of the mutex.
+
+#include<iostream>
+#include<thread>
+#include<mutex>
+
+using namespace std;
+
+std::mutex m1;
+int buffer =0;
+
+void task(const char* threadNumber,int loopfor)       //strategy 1
+{
+    std::unique_lock<mutex> lock(m1);
+    for(int i=0;i<loopfor;i++)
+    {
+        buffer++;
+        cout<<threadNumber<<" "<<buffer<<endl;
+    }
+}
+
+
+void task(const char* threadNumber,int loopfor)     //strategy 2
+{
+    std::unique_lock<mutex> lock(m1,std::defer_lock); //Does not call lock on mutex m1, because used defer_lock
+
+    lock.lock();//but then we will have to explicitly tell to lock when ever we want to lock mutex m1.
+
+    for(int i=0;i<loopfor;i++)
+    {
+        buffer++;
+        cout<<threadNumber<<" "<<buffer<<endl;
+    }
+    //lock.unlock();  is not needed as it will be unlocked in destructor of unique_lock.
+}
+
+int main()
+{
+    thread t1(task,"T1",10);
+    thread t2(task,"T2",10);
+
+    t1.join();
+    t2.join();
+    return 0;
+}
